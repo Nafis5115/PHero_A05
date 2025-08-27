e5ea88b24a7d659970db2b96f6b648f5d0413bdd
Nafis Shahriar <shaan@Nafiss-MacBook-Air.local>
2025-08-27 20:19:43 +0600

    heart button functionality completed

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,16 @@
-console.log("hello JS");
+const callButtons = document.querySelectorAll("#call-btn");
+callButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    const card = this.closest("#card");
+    const title = card.querySelector("#title").innerText;
+    const number = card.querySelector("#number").innerText;
+    alert(`📞 Calling ${title}  ${number}...`);
+  });
+});
+
+const heartButtons = document.querySelectorAll("#heart-btn");
+heartButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    let heartCount = document.getElementById("heart-count").innerText++;
+  });
+});
