4ec94f3047ddda5b03519a9f267344c712c560b6
Nafis Shahriar <shaan@Nafiss-MacBook-Air.local>
2025-08-27 21:16:02 +0600

    coin functionality completed

--- scirpt.js
+++ scirpt.js
@@ -4,13 +4,38 @@ callButtons.forEach((button) => {
     const card = this.closest("#card");
     const title = card.querySelector("#title").innerText;
     const number = card.querySelector("#number").innerText;
-    alert(`📞 Calling ${title}  ${number}...`);
+
+    let coin = document.getElementById("coin").innerText;
+
+    if (coin == 0) {
+      alert("❌ You don't have enough coin");
+    } else {
+      document.getElementById("coin").innerText =
+        document.getElementById("coin").innerText - 20;
+      alert(`📞 Calling ${title}  ${number}...`);
+    }
   });
 });
 
 const heartButtons = document.querySelectorAll("#heart-btn");
 heartButtons.forEach((button) => {
   button.addEventListener("click", function () {
-    let heartCount = document.getElementById("heart-count").innerText++;
+    document.getElementById("heart-count").innerText++;
+  });
+});
+
+const copyButtons = document.querySelectorAll("#copy-btn");
+copyButtons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    const card = this.closest("#card");
+    const numberText = card.querySelector("#number").innerText.trim();
+    const textarea = document.createElement("textarea");
+    textarea.value = numberText;
+    textarea.style.position = "fixed";
+    textarea.style.opacity = "0";
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
   });
 });
